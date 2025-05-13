d3b1cf58b63530e1d76b73fb3262296b4ff3534c
NKukame <NKukame@jhb.dvt.co.za>
2025-05-13 11:42:11 +0200

    Add react-confetti dependency for celebratory animations

--- tenzies/src/components/Die.jsx
+++ tenzies/src/components/Die.jsx
@@ -1,9 +1,17 @@
 export default function Die(props) {
-    const styles = {
-        backgroundColor: props.isHeld ? "#59E391" : "white"
-    }
+  const styles = {
+    backgroundColor: props.isHeld ? "#59E391" : "white",
+  };
 
-    return (
-        <button style={styles}>{props.value}</button>
-    )
+  return (
+    <button
+      style={styles}
+      onClick={props.hold}
+      aria-pressed={props.isHeld}
+      aria-label={`Die with value ${props.value}, 
+            ${props.isHeld ? "held" : "not held"}`}
+    >
+      {props.value}
+    </button>
+  );
 }
